d68829a715a115752bb09c3aef1c2659fc135616
Franck Cuny <franck@fcuny.net>
2024-12-28 13:48:07 -0800

    be more explicit in the default output

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod soc;
 fn main() {
     let cpu_info = soc::SocInfo::new().unwrap();
     println!(
-        "our CPU is an {}, and we have {} cores, and {} GPU cores",
+        "our CPU is an {}, and we have {} CPU cores, and {} GPU cores",
         cpu_info.cpu_brand_name, cpu_info.num_cpu_cores, cpu_info.num_gpu_cores,
     );
 }
